ea8afb073c1f773260daf14848a9353097dd3ba4
Vince Lugli <vince.lugli@gmail.com>
2014-04-14 00:42:38 -0400

    Updated parse.py
    Added methods to parse to gather information from torrent. Attempting to
    send a GET request to announce URL. Currently not working though.
    Probably because SHA1 hash isn't in the proper order (not sorted
    properly)

--- parse.py
+++ parse.py
@@ -1,22 +1,46 @@
 from bencode import *
-torrentFile = 'C:\Users\Xialin\Documents\CS 3251\BitTorrentClient\debian-live-6.0.7-amd64-gnome-desktop.iso.torrent'
-metainfo_file = open(str(torrentFile), 'rb')
-metainfo = bdecode(metainfo_file.read())
-info = metainfo['info']
-torrentDir = info['name']
-print info
-metainfo_file.close()
+import hashlib
+import requests
 
+def client():
+    bencodeMetaInfo = get_torrent_info('E:\Downloads\BitTorrentClient\Anathema -- Vol18 [mininova].torrent');
+    announceKey = get_announce(bencodeMetaInfo)
+    #print(announceKey)
+    #print(bencodeMetaInfo)
+    encodedInfo = bencode_info(bencodeMetaInfo)
+    #print(encodedInfo)
+    sha1HashedInfo = hashlib.sha1(encodedInfo).hexdigest()
+    announceUrl = announceKey + '?port=5100&uploaded=0&downloaded=0&left=0&compact=1&peer_id=vincentlugli1.0sixty&info_hash=' + sha1HashedInfo
+    #print(announceUrl)
+    announceResponse = requests.get(announceUrl)
+    print(announceResponse.text)
+    
 
+def get_torrent_info(filename):    
+    torrentFile = filename;
+    metainfo_file = open(str(torrentFile), 'rb')
+    metainfo = bdecode(metainfo_file.read())
+    info = metainfo['info']
+    # torrentDir = info['name']
+    return metainfo
+    # metainfo_file.close()
 
+    # info = metainfo['announce']
+    # print info
 
-# info = metainfo['announce']
-# print info
+    # info = metainfo['info']
+    # torrentDir = info['name']
+    # print torrentDir
 
-# info = metainfo['info']
-# torrentDir = info['name']
-# print torrentDir
+    # This one I have output to a file called test.txt
+    # info = metainfo['info']
+    # print info
 
-# This one I have output to a file called test.txt
-# info = metainfo['info']
-# print info
+def get_announce(metainfo):
+    return metainfo['announce']
+
+def bencode_info(info):
+    encodedInfo = bencode(info)
+    return encodedInfo
+
+client()
