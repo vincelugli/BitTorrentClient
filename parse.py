4cc0f13a04fdd5055d8eecb04f9e67af3a685609
Vince Lugli <vince.lugli@gmail.com>
2014-04-14 03:39:45 -0400

    Working GET request to tracker
    Gotten a list of peers (I think?) and working GET request

--- parse.py
+++ parse.py
@@ -4,27 +4,29 @@ import hashlib
 import requests
 
 def client():
-    bencodeMetaInfo = get_torrent_info('E:\Downloads\BitTorrentClient\Anathema -- Vol18 [mininova].torrent')
-    #bencodeMetaInfo = get_torrent_info('E:\Downloads\BitTorrentClient\debian-live-6.0.7-amd64-gnome-desktop.iso.torrent')
+    filename = 'E:\Downloads\BitTorrentClient\Anathema -- Vol18 [mininova].torrent'
+    #filename = 'E:\Downloads\BitTorrentClient\debian-live-6.0.7-amd64-gnome-desktop.iso.torrent'
+    bencodeMetaInfo = get_torrent_info(filename)
     #print(bencodeMetaInfo)
     announceKey = get_announce(bencodeMetaInfo)
     #print(announceKey)
     length = get_length(bencodeMetaInfo)
-    print(length)
+    #print(length)
     infoDict = get_info(bencodeMetaInfo)
     #print(infoDict)
     encodedInfo = bencode_info(infoDict)
+    #encodedInfo = get_info_from_torrent(open(filename, 'rb').read())
     #print(encodedInfo)
-    sha1HashedInfo = hashlib.sha1(encodedInfo).hexdigest()
+    sha1HashedInfo = hashlib.sha1(encodedInfo).digest()
     announceUrl = announceKey + '?info_hash=' + sha1HashedInfo + '&peer_id=vincentlugli1.0sixty&port=5100&uploaded=0&downloaded=0&left=' + str(length) + '&compact=1'
     print(announceUrl)
-    #announceResponse = requests.get(announceUrl)
-    #print(announceResponse.status_code)
+    announceResponse = requests.get(announceUrl)
+    print(announceResponse.status_code)
+    print(bdecode(announceResponse.content))
     
 
-def get_torrent_info(filename):    
-    torrentFile = filename;
-    metainfo_file = open(str(torrentFile), 'rb')
+def get_torrent_info(filename):
+    metainfo_file = open(str(filename), 'rb')
     metainfo = bdecode(metainfo_file.read())
     info = metainfo['info']
     # torrentDir = info['name']
@@ -61,5 +63,38 @@ def get_length(metainfo):
 def bencode_info(info):
     encodedInfo = bencode(info)
     return encodedInfo
+
+def get_info_from_torrent(torrent):
+    print(torrent)
+    infoLocation = torrent.find('info')
+    done = False
+    count = 1
+    currChar = infoLocation+4
+    result = ''
+    while done == False:
+        result += torrent[currChar]
+        currChar += 1
+        if (torrent[currChar] == 'i'):
+            while (torrent[currChar] != 'e'):
+                result += torrent[currChar]
+                currChar += 1
+            result += torrent[currChar]
+            print('\nRESULT:' + result + '\n')
+            currChar += 1
+        elif (torrent[currChar] == 'l'):
+            count += 1
+        elif (torrent[currChar] == 'd'):
+            count += 1
+        elif (torrent[currChar].isdigit()):
+            end = currChar + int(torrent[currChar]) + 2
+            result += torrent[currChar : end]
+            currChar = end
+        elif (torrent[currChar] == 'e'):
+            count -= 1
+        if (count == 0):
+            result += torrent[currChar]
+            done = True
+    print(result)
+    return result
 
 client()
