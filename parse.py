d08b9b43219ada6d612d14ea841982dfbae2cf56
Vince Lugli <vince.lugli@gmail.com>
2014-04-14 02:10:04 -0400

    Added support for multiple files or single files
    Can now determine the length of single files and multiple files in the
    torrent.

--- parse.py
+++ parse.py
@@ -1,23 +1,25 @@
+# -*- coding: cp1252 -*-
 from bencode import *
 import hashlib
 import requests
 
 def client():
-    bencodeMetaInfo = get_torrent_info('E:\Downloads\BitTorrentClient\Anathema -- Vol18 [mininova].torrent');
-    announceKey = get_announce(bencodeMetaInfo)
-    length = get_length(bencodeMetaInfo)
-    #print(announceKey)
+    bencodeMetaInfo = get_torrent_info('E:\Downloads\BitTorrentClient\Anathema -- Vol18 [mininova].torrent')
+    #bencodeMetaInfo = get_torrent_info('E:\Downloads\BitTorrentClient\debian-live-6.0.7-amd64-gnome-desktop.iso.torrent')
     #print(bencodeMetaInfo)
-    #print(length)
+    announceKey = get_announce(bencodeMetaInfo)
+    #print(announceKey)
+    length = get_length(bencodeMetaInfo)
+    print(length)
     infoDict = get_info(bencodeMetaInfo)
     #print(infoDict)
     encodedInfo = bencode_info(infoDict)
     #print(encodedInfo)
     sha1HashedInfo = hashlib.sha1(encodedInfo).hexdigest()
-    announceUrl = announceKey + '?port=5100&uploaded=0&downloaded=0&left=' + str(length) + '&compact=1&peer_id=vincentlugli1.0sixty&info_hash=' + sha1HashedInfo
+    announceUrl = announceKey + '?info_hash=' + sha1HashedInfo + '&peer_id=vincentlugli1.0sixty&port=5100&uploaded=0&downloaded=0&left=' + str(length) + '&compact=1'
     print(announceUrl)
-    announceResponse = requests.get(announceUrl)
-    print(announceResponse.status_code)
+    #announceResponse = requests.get(announceUrl)
+    #print(announceResponse.status_code)
     
 
 def get_torrent_info(filename):    
@@ -47,11 +49,14 @@ def get_info(metainfo):
     return metainfo['info']
 
 def get_length(metainfo):
-    files = metainfo['info']['files']
-    total = 0
-    for filePart in files:
-        total += filePart['length']
-    return total
+    if ('files' in metainfo['info']):
+        files = metainfo['info']['files']
+        total = 0
+        for filePart in files:
+            total += filePart['length']
+        return total        
+    else:
+        return metainfo['info']['length']
 
 def bencode_info(info):
     encodedInfo = bencode(info)
